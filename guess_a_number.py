a1716434f1487d04bddfc821962b4a8278312244
Barabar4e <111492110+Barabar4e@users.noreply.github.com>
2023-01-24 14:54:39 +0200

    New options in game
    New colores and bullseye optin

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,19 +1,27 @@
 import random
 
-cpu_number = random.randint(1, 100)
-
 # Read Player's Move
+cpu_number = random.randint(1, 100)
+counter_of_guesses = 0
+# Setting the body of Game
 while True:
-	player_input = input("Guess a n umber between 1 and 100: ")
-
+	# initialising player's input and changing the color of text for better vision
+	player_input = input("\33[2;34;2m Guess a number between 1 and 100: ")
+	# Checking for a proper input data from player
 	if not player_input.isdigit():
-		print("Invalid input. Try again..... ")
+		print("\33[2;31;2mInvalid input. Try again..... ")
 		continue
 	player_input = int(player_input)
+	counter_of_guesses += 1
+	# checking if the first guess is in bullseye
+	if player_input == cpu_number and counter_of_guesses == 1:
+		print("\33[1;32;2m !!! B U L L S E Y E !!! \n ")
+		break
+	# checking if the guess is correct or lower or higher than the random computer number
 	if player_input == cpu_number:
-		print("You guess it !")
+		print("\33[1;32;2m You guess it !")
 		break
 	elif player_input < cpu_number:
-		print("Too Low")
+		print("\33[1;35;20m Too Low")
 	else:
-		print("Too High")
+		print("\33[1;35;20mToo High")
